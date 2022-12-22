3d7f0482cd1805ec7aca47fd84f720f71b6a65ad
ramdevsharma-kiwi <ram.sharma@kiwitech.com>
2022-12-22 16:39:07 +0530

    created sign u

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,5 +1,53 @@
+const User = require("../models/user")
+
+const { userInfo } = require("os");
+
 module.exports.profile = function(req,res){
     return res.render('user',{
-        title:"User"
+        title:"User Profile"
     });
 }
+
+// render sign up
+module.exports.SignUp = function(req,res){
+    return res.render('user-signup',{
+        title:"Codeial | Sign Up"
+    });
+}
+
+// render sign in
+module.exports.SignIn = function(req,res){
+    return res.render('user-signin',{
+        title:"Codeial | Sign In"
+    });
+}
+
+
+//  get the sign up data
+
+module.exports.create = function(req,res){
+    if (req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email: req.body.email},function(err,user){
+        if (err){console.log(`Error in finding in signing up`)
+    return}
+    if (!user){
+        User.create(req.body,function(err,user){
+            if (err){console.log(`Error in creating user in signing up`)
+            return}
+            return res.redirect('/users/sign-in')
+        })
+    }
+    else{
+        return res.redirect('back');
+    }
+    })
+}
+
+// sign in and create the session for the user
+
+module.exports.createSession= function(req,res){
+
+}
+    
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,10 @@ const router = express.Router();
 const usersController =  require('../controllers/users-controller')
 router.get('/profile',usersController.profile)
 
+router.get('/sign-in',usersController.SignIn);
+
+router.get('/sign-up',usersController.SignUp);
+ 
+router.post('/create',usersController.create);
 
 module.exports = router;
