e42a187cb09eff1524bfa168e2d092808962e6da
ramdevsharma-kiwi <ram.sharma@kiwitech.com>
2022-12-22 13:02:20 +0530

    create folders and some routes

---
+++  index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express();
+const port = 8000;
+
+//  Use express router 
+app.use('/',require('./routes/index'));
+
+// set up the view engine 
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
+
+app.listen(port,function(err){
+    if (err){
+        console.log(`Error in running the server port: ${port}`)
+    }
+    console.log(`server is running on port: ${port}`)
+})
